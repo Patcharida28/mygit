10851273bb2208b9c987da404eef558502f77d4a
Patcharida28 <130207000+Patcharida28@users.noreply.github.com>
2023-04-08 15:28:23 +0700

    Update Home.py

--- Home.py
+++ Home.py
@@ -77,12 +77,12 @@ if st.button("ทำนายผล"):
    
    out=Knn_model.predict(x_input)
 
-    if out[0] == 'Setosa':
-        st.image("./pic/Setosa.jpg")
-    elif out[0] == 'versicolor':
-        st.image("./pic/versicolor.jpg")
-    else:
-        st.image("./pic/virginica.jpg")
+if out[0] == 'Setosa':
+    st.image("./pic/Setosa.jpg")
+   elif out[0] == 'versicolor':    
+    st.image("./pic/versicolor.jpg")
+   else:
+    st.image("./pic/virginica.jpg")
 
 else:
    st.write("ไม่แสดงข้อมูล")
