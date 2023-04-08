9d7580ee7af95a7cc6926d1ec1163c399d761b3b
Patcharida28 <130207000+Patcharida28@users.noreply.github.com>
2023-04-08 14:55:11 +0700

    Update Home.py

--- Home.py
+++ Home.py
@@ -65,8 +65,8 @@ if st.button("ทำนายผล"):
    if out[0] == 'Setosa':
     st.image("./pic/Setosa.jpg")
    else:   
-    st.writ("./pic/versicolor.jpg") 
-    else:   
+  st.writ("./pic/versicolor.jpg") 
+   else:   
     st.writ('xxx')    
    #st.button("ไม่แสดงข้อมูล")
 else:
